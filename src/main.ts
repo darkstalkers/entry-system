a195c4a7e2c1733d9abbced895bb1198551eecc9
pikotea <pikotea@gmail.com>
2017-11-23 17:24:44 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import Vue, { ComponentOptions } from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+});
